8870672f591072de01f03c11c8bdb30429732a4d
Jesse Waters <Jesse Waters>
2017-01-25 20:58:47 -0500

    Jesse Edited Antlab Main

--- M2/M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("echen67");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Jesse Waters");
+                  toPrint += p5.toString("jwaters33");
 
         System.out.println(toPrint);
 
